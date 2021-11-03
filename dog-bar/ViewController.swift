6b88045481f4087a0d809ef6fe0480b21bd74a90
Nick McDonald <nmcdonaldfhs@gmail.com>
2021-11-02 20:14:41 -0700

    Initial Commit

---
+++ dog-bar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  dog-bar
+//
+//  Created by Nicholas McDonald on 11/2/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
